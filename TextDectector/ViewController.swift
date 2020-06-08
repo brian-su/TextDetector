d7adf2773b17c500910a8a37f2e116824daf43ac
Brian <brian-su@users.noreply.github.com>
2020-06-08 15:14:36 +0100

    Initial Commit

---
+++ TextDectector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextDectector
+//
+//  Created by Brian Surgenor on 08/06/2020.
+//  Copyright © 2020 Brian Surgenor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
